7e6c609b346b6ad420a00134d44e09d3ca5f3b29
Tamara Tell <2telltamara@gmail.com>
2023-04-12 19:10:28 -0400

    added setupInput function to play.js

--- client.js
+++ client.js
@@ -23,3 +23,9 @@ const connect = function() {
 module.exports = {
   connect,
 };
+
+//Move Commands
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
--- play.js
+++ play.js
@@ -11,6 +11,19 @@
 
 const { connect } = require("./client.js");
 
-//
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (handleUserInput) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+
 
 connect();
